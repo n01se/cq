506f706cb6867978fb541260a5f7ddf808958ae9
Chouser <chouser@n01se.net>
2021-04-06 22:21:51 +0000

    take4: first attempt at two passes
    Co-authored-by: jonathanclaggett <jonathanclaggett@penguin>

--- src/net/n01se/cq/take4.clj
+++ src/net/n01se/cq/take4.clj
@@ -28,14 +28,30 @@
             env/*env*         (ana-jvm/global-env)]
     (ana/analyze form env)))
 
+;; This works with :walk :pre because that is a different pass when list-stream
+;; is :walk :post.  Does `schedule` do things in the wrong order when :walk :post are composed?
+(defn mark-invoke-target
+  {:pass-info {:walk :pre :depends #{} :after #{}}}
+  [{:keys [op] :as ast}]
+  (case op
+    :invoke (assoc-in ast [:fn :invoke-target] true)
+
+    ;; default
+    ast))
+
+;; TODO: use ana/analyze instead of our analyze below:
 (defn lift-stream
   "Lift all invokations (expressions) to return streams unless already marked as
   a stream"
-  {:pass-info {:walk :post :depends #{} :after #{}}}
+  {:pass-info {:walk :post
+               :depends #{#'mark-invoke-target}
+               :after #{}}}
   [{:keys [op form args] :as ast}]
   (case op
 
-    (:maybe-class :const) (analyze (vector form) (ana/empty-env))
+    (:maybe-class :const) (if (:invoke-target ast)
+                            ast
+                            (analyze (vector form) (ana/empty-env)))
     :invoke (do
               (clojure.pprint/pprint (-> ast :fn))
               (if (-> ast :fn :meta ::stream-aware)
@@ -47,18 +63,28 @@
     ;; default:
     ast))
 
-(defn cq* [form]
+(defn top-analyze [form]
   (-> form
       (analyze (ana/empty-env))
-      ((schedule #{#'lift-stream}))
-      emit-form))
+      ((schedule #{#'mark-invoke-target #'lift-stream}))))
+
+(defn cq* [form]
+  (emit-form (top-analyze form)))
 
 (defmacro cq [form] (cq* form))
 
 (comment
-  (analyze '(+ 5 10) (ana/empty-env))
+  (analyze '(& 5 10) (ana/empty-env))
   (emit-form (analyze '(let [x 5] (def x 10) x) (ana/empty-env)))
 
-  (cq* '(+ ^::stream x 10))
+  (cq* '(+ (& 5 6) 10))
+
+  (top-analyze '(& 5 6))
+
+
+  (cq (map #(+ 10 %) [4 5 (& 1 2)]))
+  ;;=> ((14 15 11) (14 15 12))
+
+  (schedule #{#'mark-invoke-target #'lift-stream} {:debug? true})
 
   :end)
